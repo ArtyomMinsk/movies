498d0dd70a50b07ad8e27ca09a0a41b99330db80
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-08-16 17:22:22 -0400

    data successfully migrated

--- movieratings/movieratings/secrets.py
+++ movieratings/movieratings/secrets.py
@@ -4,6 +4,7 @@ SECRET_KEY = 'ga0c$9bzy#+nhy-f$3r@r=j8b0yexhxd)m_^rpoa#8o+hjzy#m'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+RAW_DATA_FILE_PATH = "/Users/nrogers/Desktop/IronYard/movies/ml-1m/movies.dat"
 
 DATABASES = {
     'default': {
--- movieratings/movies/models.py
+++ movieratings/movies/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=500)
+
+    def __str__(self):
+        return "{}: {}".format(self.id, self.title)
+
+
+class Rater(models.Model):
+    age = models.IntegerField(null=True)
+    gender = models.CharField(max_length=1)
+    occupation = models.CharField(max_length=200) # TODO: Make sure our migration accounts for this.
+    zip_code = models.CharField(max_length=10)
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return "{}".format(self.id)
+
+
+class Rating(models.Model):
+    timestamp = models.DateTimeField(default=timezone.now)
+    score = models.IntegerField()
+    rater = models.ForeignKey(Rater)
+    movie = models.ForeignKey(Movie)
+
+    def __str__(self):
+        return "{}: {}".format(self.movie.title, self.score)
