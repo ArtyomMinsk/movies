53b9999554efa252b35ced8854964629b09e4bc2
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-08-16 19:36:43 -0400

    removed login and logout functions from views.py

--- movieratings/movies/views.py
+++ movieratings/movies/views.py
@@ -4,29 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
     pass
-
-
-def logout_view(request):
-    logout(request)
-    # Redirect to a success page.
-
-
-# def login_view(request):
-#     if request.GET:
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = authenticate(username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             # Redirect to a success page.
-#         else:
-#
-#             print('')
-#
-#             # Return an 'invalid login' error message.
-#
-#     return render(request, 'movies/index.html')
-
+    
 
 def movie_view(request):
     pass
