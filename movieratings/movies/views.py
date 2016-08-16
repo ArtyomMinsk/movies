d05065a8dd81e4e29fea495de760bbe9cf6ecfd6
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-08-16 18:09:34 -0400

    added base.html

--- movieratings/movies/views.py
+++ movieratings/movies/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
@@ -11,7 +11,17 @@ def logout_view(request):
 
 
 def login_view(request):
-    pass
+    if request.GET:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            # Return an 'invalid login' error message.
+
+    # return render(request, 'movies/index.html')
 
 
 def movie_view(request):
