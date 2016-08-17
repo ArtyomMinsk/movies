077fc98c0177279f003e4049f2d350e8b60d0345
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-08-17 00:30:24 -0400

    Updated movie_view and user_view and corresponding url's and html files

--- movieratings/movies/views.py
+++ movieratings/movies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Count
 from .models import Movie, Rater, Rating
+from django.http import Http404
 
 
 # Create your views here.
@@ -23,16 +24,38 @@ def index(request):
 
 
 def movie_view(request):
-    movies = Movie.objects.all()
+    all_movies = Movie.objects.all()
     context = {
-        'Movies': movies,
+        'all_movies': all_movies,
     }
     return render(request, 'movies/movie_view.html', context)
 
 
-def user_view(request):
-    users = Rater.objects.all()
+def movie_detail(request, movie_id):
+    try:
+        movie = Movie.objects.get(pk=movie_id)
+    except Movie.DoesNotExist:
+        raise Http404("Movie doesn't exist")
     context = {
-        'Users': users,
+        'movie': movie,
+    }
+    return render(request, 'movies/movie_detail.html', context)
+
+
+def user_view(request):
+    all_raters = Rater.objects.all()
+    context = {
+        'all_raters': all_raters,
     }
     return render(request, 'movies/user_view.html', context)
+
+
+def user_detail(request, rater_id):
+    try:
+        rater = Rater.objects.get(pk=rater_id)
+    except Rater.DoesNotExist:
+        raise Http404("Rater doesn't exist")
+    context = {
+        'rater': rater,
+    }
+    return render(request, 'movies/user_detail.html', context)
