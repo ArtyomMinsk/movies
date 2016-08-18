14c3de21412f60e121acfa4e1645fea368f88fd0
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-08-18 13:34:38 -0400

    Fixed the issue with urls for movie and user details

--- movieratings/movies/urls.py
+++ movieratings/movies/urls.py
@@ -9,7 +9,7 @@ app_name = 'movies'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^movie/$', views.movie_view, name='movie_view'),
-    url(r'^(?P<movie_id>[0-9]+)/$', views.movie_detail, name='movie_detail'),
+    url(r'^movie/(?P<movie_id>[0-9]+)/$', views.movie_detail, name='movie_detail'),
     url(r'^rater/$', views.user_view, name='user_view'),
     url(r'^rater/(?P<rater_id>[0-9]+)/$', views.user_detail, name='user_detail'),
 ]
