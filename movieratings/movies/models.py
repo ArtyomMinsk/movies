456125954b802df15c526cc27a2f1d40f2bf2263
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-08-18 18:32:54 -0400

    added average methods to models

--- movieratings/movies/models.py
+++ movieratings/movies/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.db.models import Count, Avg
 from django.contrib.auth.models import User
 from django.utils import timezone
-from statistics import mean
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
@@ -11,13 +12,23 @@ class Movie(models.Model):
         return "{}: {} {}".format(self.id, self.title, self.genre)
 
     def get_average_score(self):
-        return mean([rating.score for rating in self.rating_set.all()])    
+        if len(self.rating_set.all()) > 0:
+            self.rating_set.all().aggregate(Avg('score'))['score__avg']
+        else:
+            return "Has not been rated"
+
+    def get_average_scores(n=20):
+            return Movie.objects.annotate(
+                num_ratings=Count('rating')).filter(
+                num_ratings__gt=10).annotate(
+                average_rating=Avg('rating__score')).values(
+                    'title', 'average_rating').order_by('-average_rating')[:n]
 
 
 class Rater(models.Model):
     age = models.IntegerField(null=True)
     gender = models.CharField(max_length=1)
-    occupation = models.CharField(max_length=200) # TODO: Make sure our migration accounts for this.
+    occupation = models.CharField(max_length=200)  # TODO: Make sure our migration accounts for this.
     zip_code = models.CharField(max_length=10)
     user = models.OneToOneField(User)
 
@@ -25,7 +36,17 @@ class Rater(models.Model):
         return "{}. Age:{} Gender:{}".format(self.id, self.age, self.gender)
 
     def get_average_score(self):
-        return mean([rating.score for rating in self.rating_set.all()])
+        if len(self.rating_set.all()) > 0:
+            self.rating_set.all().aggregate(Avg('score'))['score__avg']
+        else:
+            return "Has not been rated"
+
+    def get_average_scores(n=20):
+        return Rater.objects.annotate(
+            num_ratings=Count('rating')).filter(
+            num_ratings__gt=10).annotate(
+            average_rating=Avg('rating__score')).values(
+                'id', 'average_rating').order_by('-average_rating')[:n]
 
 
 class Rating(models.Model):
