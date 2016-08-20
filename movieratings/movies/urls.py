e15bc786457334ee2089ad64bc68433df3450902
Jon Alligood <j.alligood47@gmail.com>
2016-08-20 01:06:51 -0400

    Cached index view for performance

--- movieratings/movies/urls.py
+++ movieratings/movies/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 from . import views
 
 app_name = 'movies'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', cache_page(60 * 15)(views.index), name='index'),
     url(r'^movie/$', views.movie_view, name='movie_view'),
     url(r'^movie/(?P<movie_id>[0-9]+)/$',
         views.movie_detail, name='movie_detail'),
